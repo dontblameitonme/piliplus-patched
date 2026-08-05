2713fb591cd4eab0816d9e20ebeb0a36241f1550
ci <example@example.com>
2026-08-05 13:24:32 +0800

    Upload PiliPlus patched source

---
+++ lib/pages/rcmd/view.dart
@@ -0,0 +1,165 @@
+import 'package:PiliPlus/common/skeleton/video_card_v.dart';
+import 'package:PiliPlus/common/style.dart';
+import 'package:PiliPlus/common/widgets/flutter/refresh_indicator.dart';
+import 'package:PiliPlus/common/widgets/loading_widget/http_error.dart';
+import 'package:PiliPlus/common/widgets/video_card/video_card_v.dart';
+import 'package:PiliPlus/http/loading_state.dart';
+import 'package:PiliPlus/pages/rcmd/controller.dart';
+import 'package:PiliPlus/utils/grid.dart';
+import 'package:PiliPlus/utils/storage_pref.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RcmdPage extends StatefulWidget {
+  const RcmdPage({super.key});
+
+  @override
+  State<RcmdPage> createState() => _RcmdPageState();
+}
+
+class _RcmdPageState extends State<RcmdPage>
+    with AutomaticKeepAliveClientMixin {
+  final RcmdController controller = Get.put(RcmdController());
+  bool _loadMoreScheduled = false;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  void _scheduleLoadMore() {
+    if (_loadMoreScheduled) return;
+    _loadMoreScheduled = true;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadMoreScheduled = false;
+      if (mounted) controller.onLoadMore();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    final colorScheme = ColorScheme.of(context);
+    return Container(
+      clipBehavior: .hardEdge,
+      margin: const .symmetric(horizontal: Style.safeSpace),
+      decoration: const BoxDecoration(borderRadius: Style.mdRadius),
+      child: refreshIndicator(
+        onRefresh: controller.onRefresh,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final gridDelegate = _gridDelegate(constraints.maxWidth);
+            return CustomScrollView(
+              controller: controller.scrollController,
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: [
+                SliverPadding(
+                  padding: const .only(top: Style.cardSpace, bottom: 100),
+                  sliver: Obx(
+                    () => _buildBody(
+                      colorScheme,
+                      controller.loadingState.value,
+                      gridDelegate,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  SliverGridDelegateWithExtentAndRatio _gridDelegate(double availableWidth) {
+    final isTablet = MediaQuery.sizeOf(context).shortestSide >= 600;
+    final maxCrossAxisExtent = isTablet
+        ? (availableWidth - Style.cardSpace * 3) / 4
+        : Pref.recommendCardWidth;
+    return SliverGridDelegateWithExtentAndRatio(
+      mainAxisSpacing: Style.cardSpace,
+      crossAxisSpacing: Style.cardSpace,
+      maxCrossAxisExtent: maxCrossAxisExtent,
+      childAspectRatio: Style.aspectRatio,
+      mainAxisExtent: MediaQuery.textScalerOf(context).scale(90),
+    );
+  }
+
+  Widget _buildBody(
+    ColorScheme colorScheme,
+    LoadingState<List<dynamic>?> loadingState,
+    SliverGridDelegateWithExtentAndRatio gridDelegate,
+  ) {
+    return switch (loadingState) {
+      Loading() => _buildSkeleton(gridDelegate),
+      Success(:final response) =>
+        response != null && response.isNotEmpty
+            ? SliverGrid.builder(
+                gridDelegate: gridDelegate,
+                itemBuilder: (context, index) {
+                  if (index == response.length - 1) {
+                    _scheduleLoadMore();
+                  }
+                  if (controller.lastRefreshAt != null) {
+                    if (controller.lastRefreshAt == index) {
+                      return GestureDetector(
+                        onTap: () => controller
+                          ..animateToTop()
+                          ..onRefresh(),
+                        child: Card(
+                          child: Container(
+                            alignment: Alignment.center,
+                            padding: const .symmetric(horizontal: 10),
+                            child: Text(
+                              '上次看到这里\n点击刷新',
+                              textAlign: .center,
+                              style: TextStyle(
+                                color: colorScheme.onSurfaceVariant,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    final actualIndex = index > controller.lastRefreshAt!
+                        ? index - 1
+                        : index;
+                    return VideoCardV(
+                      videoItem: response[actualIndex],
+                      onRemove: () {
+                        if (controller.lastRefreshAt != null &&
+                            actualIndex < controller.lastRefreshAt!) {
+                          controller.lastRefreshAt =
+                              controller.lastRefreshAt! - 1;
+                        }
+                        controller.loadingState
+                          ..value.data!.removeAt(actualIndex)
+                          ..refresh();
+                      },
+                    );
+                  } else {
+                    return VideoCardV(
+                      videoItem: response[index],
+                      onRemove: () => controller.loadingState
+                        ..value.data!.removeAt(index)
+                        ..refresh(),
+                    );
+                  }
+                },
+                itemCount: controller.lastRefreshAt != null
+                    ? response.length + 1
+                    : response.length,
+              )
+            : HttpError(onReload: controller.onReload),
+      Error(:final errMsg) => HttpError(
+        errMsg: errMsg,
+        onReload: controller.onReload,
+      ),
+    };
+  }
+
+  Widget _buildSkeleton(SliverGridDelegateWithExtentAndRatio gridDelegate) =>
+      SliverGrid.builder(
+        gridDelegate: gridDelegate,
+        itemBuilder: (context, index) => const VideoCardVSkeleton(),
+        itemCount: 10,
+      );
+}
